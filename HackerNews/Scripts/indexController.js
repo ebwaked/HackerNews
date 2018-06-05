e68b352066635dcee785e4829491f3a91d6f62af
Edward Waked <ebwaked@hotmail.com>
2018-06-05 07:58:35 -0400

    a mess of code, cant figure out why im getting all old stories

--- HackerNews/Scripts/indexController.js
+++ HackerNews/Scripts/indexController.js
@@ -6,28 +6,51 @@ app.controller('myCtrl', [
 
         var storyIds = [];
         var promises = [];
-        $scope.allData = [];
+        var allData = [];
 
         $scope.init = function () {
-            $http.get('https://hacker-news.firebaseio.com/v0/topstories.json').
+            $http.get('https://hacker-news.firebaseio.com/v0/askstories').
                 then(function (response) {
                     storyIds = response.data;
 
                     for (i = 0; i < storyIds.length; i++) {
+                        var ref = 
                         promises.push($http.get('https://hacker-news.firebaseio.com/v0/item/' + i + '.json'));
                     }
 
                     $q.all(promises).then(function (data) {
                         for (var i = 0; i < data.length; i++) {
                             if (data[i].data != null) {
-                                $scope.allData.push(
+                                allData.push(
                                     data[i].data
                                 );
                             }
                         }
-                        console.log($scope.allData);
+                        //var sortedData = _.sortBy(allData, 'time');
+                        //var onlyStories = _.where(sortedData, { type: "story" });
+                        //$scope.finalData = onlyStories.slice(0, 25);
+                        $scope.finalData = allData;
+                        //$scope.finalData = onlyStories.slice(onlyStories.length - 25, onlyStories.length + 1);
                     })
+
+                    
+                    
                 });
+
+            
         };
+
+        function dynamicSort(property) {
+            var sortOrder = 1;
+            if (property[0] === "-") {
+                sortOrder = -1;
+                property = property.substr(1);
+            }
+            return function (a, b) {
+                var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+                return result * sortOrder;
+            }
+        }
+        
     }]
 );
