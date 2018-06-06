5683ae7b204c098fe572784b07a9f07c40868997
eddie <ebwaked@hotmail.com>
2018-06-05 23:39:07 -0400

    fixed data to update properly, reworked html, added search function

--- HackerNews/Scripts/indexController.js
+++ HackerNews/Scripts/indexController.js
@@ -4,18 +4,21 @@ app.controller('myCtrl', [
     '$scope', '$http', '$q',
     function ($scope, $http, $q) {
 
+        var baseUrl = "https://hacker-news.firebaseio.com/";
+        var apiVersion = "v0/";
         var storyIds = [];
         var promises = [];
         var allData = [];
 
         $scope.init = function () {
-            $http.get('https://hacker-news.firebaseio.com/v0/askstories').
+            console.log(baseUrl + apiVersion);
+            $http.get(baseUrl + apiVersion + 'topstories.json').
                 then(function (response) {
                     storyIds = response.data;
 
                     for (i = 0; i < storyIds.length; i++) {
                         var ref = 
-                        promises.push($http.get('https://hacker-news.firebaseio.com/v0/item/' + i + '.json'));
+                        promises.push($http.get(baseUrl + apiVersion + 'item/'+storyIds[i]+'.json'));
                     }
 
                     $q.all(promises).then(function (data) {
@@ -25,32 +28,11 @@ app.controller('myCtrl', [
                                     data[i].data
                                 );
                             }
+                            $scope.finalData = allData;
                         }
-                        //var sortedData = _.sortBy(allData, 'time');
-                        //var onlyStories = _.where(sortedData, { type: "story" });
-                        //$scope.finalData = onlyStories.slice(0, 25);
-                        $scope.finalData = allData;
-                        //$scope.finalData = onlyStories.slice(onlyStories.length - 25, onlyStories.length + 1);
                     })
-
-                    
-                    
                 });
-
-            
         };
-
-        function dynamicSort(property) {
-            var sortOrder = 1;
-            if (property[0] === "-") {
-                sortOrder = -1;
-                property = property.substr(1);
-            }
-            return function (a, b) {
-                var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-                return result * sortOrder;
-            }
-        }
         
     }]
 );
