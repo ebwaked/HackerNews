a9446a39482791b733b5ff65953aba8a99a4fb8c
eddie <ebwaked@hotmail.com>
2018-06-06 01:40:49 -0400

    updated the view, refactored some code, added caching but not working as intended

--- HackerNews/Scripts/indexController.js
+++ HackerNews/Scripts/indexController.js
@@ -6,33 +6,35 @@ app.controller('myCtrl', [
 
         var baseUrl = "https://hacker-news.firebaseio.com/";
         var apiVersion = "v0/";
+        var fullUrl = baseUrl + apiVersion;
         var storyIds = [];
         var promises = [];
         var allData = [];
+        $http.defaults.cache = true;
 
         $scope.init = function () {
-            console.log(baseUrl + apiVersion);
-            $http.get(baseUrl + apiVersion + 'topstories.json').
-                then(function (response) {
-                    storyIds = response.data;
 
-                    for (i = 0; i < storyIds.length; i++) {
-                        var ref = 
-                        promises.push($http.get(baseUrl + apiVersion + 'item/'+storyIds[i]+'.json'));
-                    }
+              
+                $http.get(fullUrl + 'topstories.json', { cache: true }).
+                    then(function (response) {
+                        storyIds = response.data;
 
-                    $q.all(promises).then(function (data) {
-                        for (var i = 0; i < data.length; i++) {
-                            if (data[i].data != null) {
-                                allData.push(
-                                    data[i].data
-                                );
+                        for (i = 0; i < storyIds.length; i++) {
+                            var ref = 
+                            promises.push($http.get(fullUrl + 'item/'+storyIds[i]+'.json', { cache: true}));
+                        }
+
+                        $q.all(promises).then(function (data) {
+                            for (var i = 0; i < data.length; i++) {
+                                if (data[i].data != null) {
+                                    allData.push(
+                                        data[i].data
+                                    );
+                                }
                             }
                             $scope.finalData = allData;
-                        }
-                    })
-                });
-        };
-        
+                        })
+                    });
+                };
     }]
 );
